221025f3de1b12fa172e609f1e31827e904b112e
Himaja Ryali <ryali.hr@gmail.com>
2023-04-24 20:30:01 +0000

    User Authentication and update the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :organic_stores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "organic_stores#index"
+  resources :organic_stores
 end
